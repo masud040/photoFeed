b59866b62731f82fcb61bd88bedcb990cea6cc87
masud040 <masudinfo040@.com>
2024-04-06 10:04:56 +0600

    set two route for get all data and get data by id

---
+++ app/api/photos/[id]/route.js
@@ -0,0 +1,6 @@
+import { getPhotoById } from "@/lib/image-data";
+import { NextResponse } from "next/server";
+export async function GET(request, { params: { id } }) {
+  const data = await getPhotoById(id);
+  return NextResponse.json(data);
+}
