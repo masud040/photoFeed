4469fd357d4a04787cf00083b8726e449fa9ac60
masud040 <masudinfo040@.com>
2024-04-06 20:58:00 +0600

    fetch data and add two file for language version

--- app/[lang]/dictionaries/dictionaries.js
+++ app/[lang]/dictionaries/dictionaries.js
@@ -1,3 +1,4 @@
+import "server-only";
 const dictionaries = {
   en: () => import("./en.json").then((module) => module.default),
   bn: () => import("./bn.json").then((module) => module.default),
--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>photo feed</div>;
+import PhotoList from "../components/PhotoList";
+
+export default async function Home() {
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json();
+
+  return (
+    <div className="container my-4 lg:my-8">
+      <PhotoList photos={photos} />
+    </div>
+  );
 }
